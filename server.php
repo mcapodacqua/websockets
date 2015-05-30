bd280578cdd2664f1c42322d33220613ee45d138
Maximiliano <maxi@softwareadvice.com>
2015-05-30 15:20:54 -0300

    WIP server file

--- server.php
+++ server.php
@@ -4,7 +4,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-
+$app_id = getenv('APP_ID');
+$app_key = getenv('APP_KEY');
+$app_secret = getenv('APP_SECRET');
 
 $pusher = new Pusher($app_key, $app_secret, $app_id);
 
+
+$pusher->trigger('test_channel','test_event',array(
+    'time' => time(),
+    'message' => 'Hi!',
+));
